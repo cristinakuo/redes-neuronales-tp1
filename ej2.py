c77a4dcd4790266ac38a32822dd827272f488a18
Cristina <ckuo@fi.uba.ar>
2019-09-23 18:16:09 -0300

    Add list of P_errors

--- ej2.py
+++ ej2.py
@@ -3,14 +3,16 @@ import numpy as np
 from tqdm import tqdm
 from progress.spinner import Spinner
 from utils import *
+from tabulate import tabulate
 
 import logging
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s - %(message)s") 
 log = logging.getLogger(__name__)
 
-def main():
-    P_error_ref = 0.001
-    N = 100
+def get_capacity_with_Perror_ref(P_error_ref):
+    
+
+    N=100
 
     log.info("Start process...")
     step_size = 10
@@ -19,30 +21,35 @@ def main():
 
     pattern_count = 0
     P_error = 0
-    with Spinner("Processing...") as bar:
-        while P_error < P_error_ref:
-            pattern_count += 1
-            patterns = gen_list_of_patterns(N,pattern_count)
+    #with Spinner("Processing...") as bar:
+    while P_error < P_error_ref:
+        pattern_count += 1
+        patterns = gen_list_of_patterns(N,pattern_count)
+        myHop = HopfieldNet()
+        for p in patterns:
+            myHop.load_pattern_arr(p)
+        myHop.train()
+        errors = []
+        for p in patterns:
+            errors.append(myHop.refresh_synchronic(p, render=False) - p)
+            
+        errors = np.vstack(errors)
+        error_count = len(np.flatnonzero(errors))
+        P_error = np.true_divide(error_count, N * pattern_count)
+            #bar.next()
+    capacity = np.true_divide(pattern_count,N)
+    
 
-            myHop = HopfieldNet()
-            for p in patterns:
-                myHop.load_pattern_arr(p)
-            myHop.train()
+    return (P_error, capacity)
 
-            errors = []
-            for p in patterns:
-                errors.append(myHop.refresh_synchronic(p, render=False) - p)
-                
-            errors = np.vstack(errors) 
-            error_count = len(np.flatnonzero(errors))
+def main():
+    P_errors_ref = [0.001, 0.0036, 0.01, 0.05, 0.1]
 
-            P_error = np.true_divide(error_count, N * pattern_count)
-            bar.next()
-    log.info("Done")
-    log.info("Error probability reached: {}/{} = {}".format(error_count,N*pattern_count, P_error))
-    log.info("Learned pattern count - Neurons ratio: {}/{} = {}".format(
-		pattern_count, N, np.true_divide(pattern_count,N)))
+    results = []
+    for p in P_errors_ref:
+        results.append(get_capacity_with_Perror_ref(p))
 
+    print(tabulate(results, headers=['P_err reached', 'Capacity']))
 
 if __name__ == '__main__':
     main()
