cb939a51d5b439b54b811a318c05eb2fa9ef19ac
Cristina <ckuo@fi.uba.ar>
2019-10-09 18:42:16 -0300

    Ising Model update

--- ej4.py
+++ ej4.py
@@ -1,1 +1,14 @@
 from ising import IsingModel
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.ion()
+
+
+def main():
+    myIsing = IsingModel(rows=4,cols=5,init_temp=20)
+    myIsing.run()
+
+
+if __name__ == '__main__':
+    main()
--- ising.py
+++ ising.py
@@ -1,47 +1,52 @@
 import numpy as np
 from utils import sgn
-
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-plt.ion()
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(levelname)s - %(message)s") 
+log = logging.getLogger(__name__)
 
 TEMPERATURE_DECREASING_FACTOR = 0.95
-TEMPERATURE_MAX_STEP_COUNT = 2048
+MAX_ITERATIONS_PER_TEMPERATURE = 2048
 
 class IsingModel():
 
     def __init__(self, rows, cols, init_temp):
         self.rows = rows
         self.cols = cols
-        # Inicializo lattice totalmente randomly
         self.lattice = np.vectorize(sgn)( np.random.rand(self.rows,self.cols), 0.5)
         self.temperature = init_temp
-        self.temperature_step_count = 0
+        self.iterations_per_temp_count = 0
         self.magnetization = self.lattice.sum()
 
-    def _bc_row(self, row_ind):
-        if (row_ind >= self.rows) or (row_ind < 0):
+    def isNotMagnetized(self):
+        if (abs(self.magnetization) == self.cols*self.rows):
+            return False
+        else:
+            return True
+
+    def _accessLattice(self,indexes):
+        row_index,col_index = indexes
+        
+        if (row_index >= self.rows) or (row_index < 0):
+            return 0
+        elif (col_index >= self.cols) or (col_index < 0):
             return 0
         else:
-            return row_ind
-    
-    def _bc_col(self, col_ind):
-        if (col_ind >= self.cols) or (col_ind < 0):
-            return 0
-        else:
-            return col_ind
+            return self.lattice[row_index,col_index]
 
-    def _update_magnetization(self):
-        self.magnetization = self.lattice.sum()
+    # Magnetization is simply the sum of all components in lattice
+    def getMagnetization(self):
+        return self.lattice.sum()
 
-    def _update_temperature(self):
-        if self.temperature_step_count == TEMPERATURE_MAX_STEP_COUNT:
+    def _updateTemperature(self):
+        if self.iterations_per_temp_count == MAX_ITERATIONS_PER_TEMPERATURE:
             self.temperature *= TEMPERATURE_DECREASING_FACTOR
-            self.temperature_step_count = 0
-            self.temperature_step_count += 1
+            self.iterations_per_temp_count = 1
         else:
-            self.temperature_step_count += 1
+            self.iterations_per_temp_count += 1
 
 
     def _getLatticeWithFlippedCandidateAt(self, index):
@@ -49,76 +54,78 @@ class IsingModel():
         new_lattice[index] *= -1
         return new_lattice
 
-    def _get_cell_energy(self, index, lattice):
+    def _getCellEnergy(self, index, lattice):
         i,j = index
         return -1/2*self.lattice[i,j]*(
-                                self.lattice[self._bc_row(i+1),j] 
-                                + self.lattice[self._bc_row(i-1),j] 
-                                + self.lattice[i,self._bc_col(j+1)]
-                                + self.lattice[i,self._bc_col(j-1)]
+                                  self._accessLattice((i+1,j))
+                                + self._accessLattice((i-1,j))
+                                + self._accessLattice((i,j+1)) 
+                                + self._accessLattice((i,j-1))
                                 )
 
-    def _get_energy(self,lattice):
+    def _getEnergy(self,lattice):
         energy = 0
         for i in range(self.rows):
             for j in range(self.cols):
-                energy = energy + self._get_cell_energy((i,j),lattice)
+                energy = energy + self._getCellEnergy((i,j),lattice)
         return energy
 
-    def _acceptance_prob(self, dE):
-        p = 0
+    def _acceptanceProb(self, dE):
         try:
-            p = np.exp(-dE/self.temperature)
+            probability = np.exp(-np.true_divide(dE,self.temperature))
+            return probability
         except ZeroDivisionError:
             log.warning("Catching ZeroDivisionError...")
-        
-        return p
+            exit()
 
-    def _updateLattice(self, index, dE):
-        if dE < 0:
+    def _acceptWithProb(self, delta_energy):
+        if (np.random.random() <= self._acceptanceProb(delta_energy) ): # random() returns value between 0 and 1
+            return True
+        else:
+            return False
+
+    def _updateLattice(self, index, delta_energy):
+        if delta_energy < 0:
             self.lattice[index] *= -1
-        elif np.random.random() <= self._acceptance_prob(dE):
+        elif self._acceptWithProb(delta_energy): 
             self.lattice[index] *= -1
        
     def _iterate(self):
+        # Choose random element in lattice and get it flipped
         index = np.random.randint(self.rows), np.random.randint(self.cols)
         new_lattice = self._getLatticeWithFlippedCandidateAt(index)
 
-        old_E = self._get_energy(self.lattice)
-        new_E = self._get_energy(new_lattice)
+        old_E = self._getEnergy(self.lattice)
+        new_E = self._getEnergy(new_lattice)
+        delta_E = new_E - old_E
+        self._updateLattice(index, delta_E)
+        self._updateTemperature()
 
-        self._updateLattice(index, new_E-old_E)
-        self._update_magnetization()
-        self._update_temperature()
+    def run(self):     
+        plt.ion()
+        _, axs = plt.subplots(nrows=2, ncols=1)
+        axs[1].set_title('Magnetization Curve')
+        axs[1].set_xlabel('Temperature: {}'.format(self.temperature))  
+        
+        while ( self.isNotMagnetized() ):
+            self._iterate()       
+            self.render_plot(axs)
+        
+        input("Press Enter to exit...")
 
-    def run(self,axs):  
-        while abs(self.magnetization != self.rows*self.cols):
-            self._iterate()
-            #self._render_plot(a)
-            self.render_plot(axs, force=False)
-        self.render_plot(axs)
-        print(self.lattice)
-
-    def render_plot(self, axs, force=True):
-        # Se renderiza unicamente si se fuerza o esta en el ultimo paso
-        if force or self.temperature_step_count == TEMPERATURE_MAX_STEP_COUNT:
+    def render_plot(self, axs):
+        # Renders image only if it's in the last step count
+        if self.iterations_per_temp_count == MAX_ITERATIONS_PER_TEMPERATURE:
             axs[0].clear()
             axs[0].set_title('Ising Model Simulation')
             axs[0].imshow(self.lattice,vmin=-1, vmax=1)
-            axs[1].semilogx(self.temperature, self.magnetization, "ob")
+            
+            axs[1].semilogx(self.temperature, self.getMagnetization(),"*b")
+            axs[1].set_xlabel('Temperature: {}'.format(self.temperature))  
             plt.show()
             plt.pause(1e-12)
 
 
 if __name__ == '__main__':
-    ismo = IsingModel(10,10,10)
-    fig, axs = plt.subplots(1, 2)
-    axs[1].set_title('Magnetization Curve')
-    axs[1].set_xlabel('Temperature')
-    
-    while abs(ismo.magnetization != ismo.rows*ismo.cols):
-        ismo._iterate()
-        #self._render_plot(a)
-        ismo.render_plot(axs, force=False)
-    ismo.render_plot(axs)
-    input("Press Enter to exit...")
+    isingModel = IsingModel(4,4,10)
+    isingModel.run()
